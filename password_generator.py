2257edc795f72742823ae0a3744d6e8e36da70ba
Ram Krishna Pandey <100858554+rkpdy23@users.noreply.github.com>
2022-08-19 11:49:21 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -19,13 +19,13 @@ def generatePassword(password_length):
 
 def replaceWithNumber(pword):
     for i in range(random.randrange(1,3)):
-        replace_index = random.randrange(len(pword)//2)
+        replace_index = random.randrange(len(pword))
         pword = pword[0:replace_index] + str(random.randrange(10)) + pword[replace_index+1:]
         return pword
 
 def replaceWithUppercaseLetter(pword):
-    for i in range(random.randrange(1,3)):
-        replace_index = random.randrange(len(pword)//2,len(pword))
+    for i in range(random.randrange(1,5)):
+        replace_index = random.randrange(len(pword))
         pword = pword[0:replace_index] + pword[replace_index].upper() + pword[replace_index+1:]
         return pword
 
@@ -39,7 +39,7 @@ def main():
        print("Minimum length of password should be 3")
 
     for i in range(numPasswords):
-        length = int(input("Enter the length of Password #" + str(i+1) + " "))
+        length = int(input("Enter the length of Password " + str(i+1) + " "))
         if length<3:
             length = 3
         passwordLengths.append(length)
@@ -47,5 +47,5 @@ def main():
         Password = generatePassword(passwordLengths)
 
     for i in range(numPasswords):
-        print ("Password #"+str(i+1)+" = " + Password[i])
+        print ("Password-"+str(i+1)+" = " + Password[i])
 main()
